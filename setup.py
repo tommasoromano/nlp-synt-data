ff8a37711347d5048faa9f79bade3c0d8f87c4a6
Tommaso Romano <34575973+tommasoromano@users.noreply.github.com>
2024-06-18 11:14:16 +0200

    prompt generator

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nlp_synt_data",
+    packages=["nlp_synt_data"],
+    version="0.0.1",
+    author="Tommaso Romano'",
+    author_email="romabob1300@gmail.com",
+    description="Synthesized Data for NLP Tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tommasoromano/nlp-synt-data",
+    license="The MIT License (MIT)",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
